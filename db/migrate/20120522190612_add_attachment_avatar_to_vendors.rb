bed720f7b8ee8e5c3e18cd4fb701d66986a65455
Jamie Gladfelter <jlgladf@gmail.com>
2012-05-22 19:25:19 -0500

    Fixed migration

--- db/migrate/20120522190612_add_attachment_avatar_to_vendors.rb
+++ db/migrate/20120522190612_add_attachment_avatar_to_vendors.rb
@@ -1,15 +1,15 @@
 class AddAttachmentAvatarToVendors < ActiveRecord::Migration
   def self.up
-    add_column :vendor, :avatar_file_name, :string
-    add_column :vendor, :avatar_content_type, :string
-    add_column :vendor, :avatar_file_size, :integer
-    add_column :vendor, :avatar_updated_at, :datetime
+    add_column :vendors, :avatar_file_name, :string
+    add_column :vendors, :avatar_content_type, :string
+    add_column :vendors, :avatar_file_size, :integer
+    add_column :vendors, :avatar_updated_at, :datetime
   end
 
   def self.down
-    remove_column :vendor, :avatar_file_name
-    remove_column :vendor, :avatar_content_type
-    remove_column :vendor, :avatar_file_size
-    remove_column :vendor, :avatar_updated_at
+    remove_column :vendors, :avatar_file_name
+    remove_column :vendors, :avatar_content_type
+    remove_column :vendors, :avatar_file_size
+    remove_column :vendors, :avatar_updated_at
   end
 end
